3ca295a7cc008a4015a472166d02988b23de140e
Thomas Lindeker <thomas.lindeker@interieur.gouv.fr>
2023-06-14 16:47:57 +0200

    movie player and responsive auth

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,81 +1,113 @@
 import React, { useEffect, useRef, useState } from "react";
-
 import {
-	PlayIcon,
-	PauseIcon,
-	ArrowLeftIcon,
+    PlayIcon,
+    PauseIcon,
+    ArrowLeftIcon,
 } from "@heroicons/react/24/outline";
 
 interface VideoPlayerProps {
-	src: string;
-	router: any;
+    src: string;
+    router: any;
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, router }) => {
-	const videoRef = useRef<HTMLVideoElement>(null);
-	const [playing, setPlaying] = useState(true);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [playing, setPlaying] = useState(false);
+    const [blobUrl, setBlobUrl] = useState("");
+    const [loading, setLoading] = useState(true); // Add a loading state
 
-	useEffect(() => {
-		const playPromise = videoRef.current?.play();
-		if (playPromise !== undefined) {
-			playPromise
-				.then(() => {
-					videoRef.current!.muted = false;
-				})
-				.catch((error) => {
-					setPlaying(false);
-				});
-		}
-	}, []);
+    useEffect(() => {
+        fetch(src)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.blob();
+            })
+            .then((blob) => {
+                const blobURL = URL.createObjectURL(blob);
+                setBlobUrl(blobURL);
+                console.log(blobURL);
+                setLoading(false); // Set loading to false after Blob URL has been created
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                setLoading(false); // Also set loading to false if there's an error
+            });
+    }, [src]);
 
-	const handlePlayPause = () => {
-		if (videoRef.current) {
-			if (playing) {
-				videoRef.current.pause();
-			} else {
-				videoRef.current.play();
-			}
-			setPlaying(!playing);
-		}
-	};
+    useEffect(() => {
+        if (blobUrl && videoRef.current) {
+            videoRef.current
+                .play()
+                .then(() => setPlaying(true))
+                .catch((error) => console.error("Video play failed:", error));
+        }
+    }, [blobUrl]);
 
-	return (
-		<div
-			className="flex items-center justify-center h-screen "
-			onClick={handlePlayPause}
-		>
-			{/* pause button */}
-			<button
-				className="absolute left-0 m-4 p-2 bg-black bg-opacity-50 rounded-full bottom-0 cursor-pointer z-10"
-				onClick={handlePlayPause}
-			>
-				{playing ? (
-					<PauseIcon className="w-6 h-6 text-white" />
-				) : (
-					<PlayIcon className="w-6 h-6 text-white" />
-				)}
-			</button>
-			{/* exit button */}
-			<button
-				className="absolute left-0 m-4 p-2 bg-black bg-opacity-50 rounded-full top-0 cursor-pointer z-10"
-				onClick={() => {
-					router.push("/");
-				}}
-			>
-				<ArrowLeftIcon className="w-6 h-6 text-white" />
-			</button>
-			<video
-				ref={videoRef}
-				className="w-full h-full object-cover"
-				src={src}
-			/>
-			{!playing && (
-				<div className="absolute inset-0 flex items-center justify-center text-white text-4xl bg-black bg-opacity-50 ">
-					<PlayIcon className="w-10 h-10" />
-				</div>
-			)}
-		</div>
-	);
+    const handlePlayPause = () => {
+        if (videoRef.current) {
+            if (playing) {
+                videoRef.current.pause();
+            } else {
+                videoRef.current.play();
+            }
+
+            setPlaying((currentPlaying) => !currentPlaying);
+        }
+    };
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="text-4xl text-white">Loading...</h1>
+            </div>
+        ); // Render a loading indication
+    }
+
+    if (!blobUrl) {
+        return null;
+    }
+
+    return (
+        <div className="flex items-center justify-center h-screen ">
+            {/* pause button */}
+            <button
+                className="absolute left-0 m-4 p-2 bg-black bg-opacity-50 rounded-full bottom-0 cursor-pointer z-10"
+                onClick={handlePlayPause}
+            >
+                {playing ? (
+                    <PauseIcon className="w-6 h-6 text-white" />
+                ) : (
+                    <PlayIcon className="w-6 h-6 text-white" />
+                )}
+            </button>
+            {/* exit button */}
+            <button
+                className="absolute left-0 m-4 p-2 bg-black bg-opacity-50 rounded-full top-0 cursor-pointer z-10"
+                onClick={() => {
+                    router.push("/");
+                }}
+            >
+                <ArrowLeftIcon className="w-6 h-6 text-white" />
+            </button>
+            <video
+                muted
+                onClick={handlePlayPause}
+                ref={videoRef}
+                className="w-full h-full object-cover"
+                src={blobUrl}
+            />
+            {!playing && (
+                <div
+                    onClick={handlePlayPause}
+                    className="absolute inset-0 flex items-center justify-center text-white text-4xl bg-black bg-opacity-50 "
+                >
+                    <PlayIcon className="w-10 h-10" />
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default VideoPlayer;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -8,143 +8,152 @@ import { FcGoogle } from "react-icons/fc";
 import Input from "@/components/Input";
 
 export async function getServerSideProps(context: NextPageContext) {
-	const session = await getSession(context);
+    const session = await getSession(context);
 
-	if (session) {
-		return {
-			redirect: {
-				destination: "/",
-				permanent: false,
-			},
-		};
-	}
+    if (session) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
 
-	return {
-		props: {},
-	};
+    return {
+        props: {},
+    };
 }
 
 const Auth = () => {
-	const router = useRouter();
+    const router = useRouter();
 
-	const [email, setEmail] = useState("");
-	const [name, setName] = useState("");
-	const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
 
-	const [variant, setVariant] = useState("login");
+    const [variant, setVariant] = useState("login");
 
-	const toggleVariant = useCallback(() => {
-		setVariant((currentVariant) =>
-			currentVariant === "login" ? "register" : "login"
-		);
-	}, []);
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) =>
+            currentVariant === "login" ? "register" : "login"
+        );
+    }, []);
 
-	const login = useCallback(async () => {
-		try {
-			await signIn("credentials", {
-				email,
-				password,
-				redirect: false,
-				callbackUrl: "/",
-			});
+    const login = useCallback(async () => {
+        try {
+            await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: "/",
+            });
 
-			router.push("/profiles");
-		} catch (error) {
-			console.log(error);
-		}
-	}, [email, password, router]);
+            router.push("/profiles");
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password, router]);
 
-	const register = useCallback(async () => {
-		try {
-			await axios.post("/api/register", {
-				email,
-				name,
-				password,
-			});
+    const register = useCallback(async () => {
+        try {
+            await axios.post("/api/register", {
+                email,
+                name,
+                password,
+            });
 
-			login();
-		} catch (error) {
-			console.log(error);
-		}
-	}, [email, name, password, login]);
+            login();
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password, login]);
 
-	return (
-		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-			<div className="bg-black w-full h-full lg:bg-opacity-50">
-				<nav className="px-12 py-5">
-					<img src="/images/logo.png" className="h-12" alt="Logo" />
-				</nav>
-				<div className="flex justify-center">
-					<div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-						<h2 className="text-white text-4xl mb-8 font-semibold">
-							{variant === "login" ? "Sign in" : "Register"}
-						</h2>
-						<div className="flex flex-col gap-4">
-							{variant === "register" && (
-								<Input
-									id="name"
-									type="text"
-									label="Username"
-									value={name}
-									onChange={(e: any) =>
-										setName(e.target.value)
-									}
-								/>
-							)}
-							<Input
-								id="email"
-								type="email"
-								label="Email address or phone number"
-								value={email}
-								onChange={(e: any) => setEmail(e.target.value)}
-							/>
-							<Input
-								type="password"
-								id="password"
-								label="Password"
-								value={password}
-								onChange={(e: any) =>
-									setPassword(e.target.value)
-								}
-							/>
-						</div>
-						<button
-							onClick={variant === "login" ? login : register}
-							className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
-						>
-							{variant === "login" ? "Login" : "Sign up"}
-						</button>
-						<div className="flex flex-row items-center gap-4 mt-8 justify-center">
-							<div
-								onClick={() =>
-									signIn("google", {
-										callbackUrl: "/profiles",
-									})
-								}
-								className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
-							>
-								<FcGoogle size={32} />
-							</div>
-						</div>
-						<p className="text-neutral-500 mt-12">
-							{variant === "login"
-								? "First time using Netflix?"
-								: "Already have an account?"}
-							<span
-								onClick={toggleVariant}
-								className="text-white ml-1 hover:underline cursor-pointer"
-							>
-								{variant === "login"
-									? "Create an account"
-									: "Login"}
-							</span>
-							.
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div id="auth">
+            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 z-1"></div>
+            <div className="relative h-full bg-no-repeat bg-center bg-fixed bg-cover z-2">
+                <div className="relative z-3">
+                    <nav className="px-12 py-5">
+                        <img
+                            src="/images/logo.png"
+                            className="h-12"
+                            alt="Logo"
+                        />
+                    </nav>
+                    <div className="flex justify-center">
+                        <div className="bg-black bg-opacity-70 px-4 py-8 self-center mt-2 lg:w-2/5 md:px-16 md:py-16 lg:max-w-md md:rounded-md w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
+                            <h2 className="text-white text-4xl mb-8 font-semibold">
+                                {variant === "login" ? "Sign in" : "Register"}
+                            </h2>
+                            <div className="flex flex-col gap-4">
+                                {variant === "register" && (
+                                    <Input
+                                        id="name"
+                                        type="text"
+                                        label="Username"
+                                        value={name}
+                                        onChange={(e: any) =>
+                                            setName(e.target.value)
+                                        }
+                                    />
+                                )}
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    label="Email address"
+                                    value={email}
+                                    onChange={(e: any) =>
+                                        setEmail(e.target.value)
+                                    }
+                                />
+                                <Input
+                                    type="password"
+                                    id="password"
+                                    label="Password"
+                                    value={password}
+                                    onChange={(e: any) =>
+                                        setPassword(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <button
+                                onClick={variant === "login" ? login : register}
+                                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+                            >
+                                {variant === "login" ? "Login" : "Sign up"}
+                            </button>
+                            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                                <div
+                                    onClick={() =>
+                                        signIn("google", {
+                                            callbackUrl: "/profiles",
+                                        })
+                                    }
+                                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                                >
+                                    <FcGoogle size={32} />
+                                </div>
+                            </div>
+                            <p className="text-neutral-500 mt-12">
+                                {variant === "login"
+                                    ? "First time using Netflix?"
+                                    : "Already have an account?"}
+                                <span
+                                    onClick={toggleVariant}
+                                    className="text-white ml-1 hover:underline cursor-pointer"
+                                >
+                                    {variant === "login"
+                                        ? "Create an account"
+                                        : "Login"}
+                                </span>
+                                .
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Auth;
