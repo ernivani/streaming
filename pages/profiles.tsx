02e7da3760800e6c32cbd1850eb89aba02920b60
Thomas Lindeker <45493414+ernivani@users.noreply.github.com>
2023-06-16 11:19:03 +0200

    fix:  dumplicata ligne (#2)
    Co-authored-by: Thomas Lindeker <thomas.lindeker@interieur.gouv.fr>

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -3,7 +3,6 @@ import { getSession, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { useState, useEffect } from "react";
-import { useState, useEffect } from "react";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useCurrentProfile from "@/hooks/useCurrentProfile";
@@ -16,14 +15,14 @@ const images = [
 ];
 
 interface UserCardProps {
-	name: string;
-	image: string;
+    name: string;
+    image: string;
 }
 
 interface Profile {
-	id: string;
-	name: string;
-	image: string;
+    id: string;
+    name: string;
+    image: string;
 }
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
@@ -43,21 +42,21 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const UserCard: React.FC<UserCardProps> = ({ name, image }) => {
-	const [imgSrc, setImgSrc] = useState("");
+    const [imgSrc, setImgSrc] = useState("");
 
-	useEffect(() => {
-		if (image) {
-			setImgSrc(image);
-		} else {
-			// use a simple hash function to get a somewhat random, but still deterministic, index
-			let hash = 0;
-			for (let i = 0; i < name.length; i++) {
-				hash = (31 * hash + name.charCodeAt(i)) >>> 0; // >>> 0 forces to unsigned 32 bit number
-			}
-			const imageIndex = hash % images.length;
-			setImgSrc(images[imageIndex]);
-		}
-	}, [name, image]);
+    useEffect(() => {
+        if (image) {
+            setImgSrc(image);
+        } else {
+            // use a simple hash function to get a somewhat random, but still deterministic, index
+            let hash = 0;
+            for (let i = 0; i < name.length; i++) {
+                hash = (31 * hash + name.charCodeAt(i)) >>> 0; // >>> 0 forces to unsigned 32 bit number
+            }
+            const imageIndex = hash % images.length;
+            setImgSrc(images[imageIndex]);
+        }
+    }, [name, image]);
 
     useEffect(() => {
         setImgSrc(images[Math.floor(Math.random() * 4)]);
@@ -80,41 +79,41 @@ const UserCard: React.FC<UserCardProps> = ({ name, image }) => {
 };
 
 const App = () => {
-	const router = useRouter();
-	const { data: currentProfiles } = useCurrentProfile();
+    const router = useRouter();
+    const { data: currentProfiles } = useCurrentProfile();
 
     const selectProfile = useCallback(() => {
         router.push("/");
     }, [router]);
 
-	if (!currentProfiles) {
-		return null;
-	}
+    if (!currentProfiles) {
+        return null;
+    }
 
-	return (
-		<div className="flex items-center h-full justify-center">
-			<div className="flex flex-col">
-				<h1 className="text-3xl md:text-6xl text-white text-center">
-					Who&#39;s watching?
-				</h1>
-				<div className="flex items-center justify-center gap-8 mt-10">
-					<div
-						className="flex flex-wrap gap-4"
-						onClick={() => selectProfile()}
-					>
-						{/* for profile in currentProfiles */}
-						{currentProfiles.map((profile: Profile) => (
-							<UserCard
-								key={profile.id}
-								name={profile.name}
-								image={profile.image}
-							/>
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex items-center h-full justify-center">
+            <div className="flex flex-col">
+                <h1 className="text-3xl md:text-6xl text-white text-center">
+                    Who&#39;s watching?
+                </h1>
+                <div className="flex items-center justify-center gap-8 mt-10">
+                    <div
+                        className="flex flex-wrap gap-4"
+                        onClick={() => selectProfile()}
+                    >
+                        {/* for profile in currentProfiles */}
+                        {currentProfiles.map((profile: Profile) => (
+                            <UserCard
+                                key={profile.id}
+                                name={profile.name}
+                                image={profile.image}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
